d2a181d7eec779cac58c5d43f7ffaa5aea1031bf
charlyli17 <kilarichristine@gmail.com>
2022-02-13 22:03:33 +0100

    Commit fonction add individu

--- src/engine/process/EvolutionElementManager.java
+++ src/engine/process/EvolutionElementManager.java
@@ -11,5 +11,13 @@ public class EvolutionElementManager {
 		int age = (int) (Math.random() * (100 + 1 - 0)) + 0;
 		individu.setAge(age);
 	}
+
+	public ArrayList<Individu> getIndividus() {
+		return individus;
+	}
+	
+	public void add(Individu individu) {
+		individus.add(individu);
+	}
 	
 }
