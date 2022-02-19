1293f91ecc7976c94f990af4ccfae6673f2d12fc
charlyli17 <kilarichristine@gmail.com>
2022-02-19 21:17:47 +0100

    Test sur la génération de nom et prénom

---
+++ src/test/Main.java
@@ -0,0 +1,17 @@
+package test;
+
+import java.io.File;
+import java.io.IOException;
+import engine.process.EvolutionElementManager;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		String file = "/Users/charlylin/eclipse-workspace2/social/bin/fichiers/noms individus social.csv";
+		File f = new File(file);
+		String nom = EvolutionElementManager.generateNom(f);
+		String prenom = EvolutionElementManager.generatePrenom(f, "Feminine");
+		System.out.println("Last name = " + nom + " ,First Name = " + prenom);
+	}
+
+}
