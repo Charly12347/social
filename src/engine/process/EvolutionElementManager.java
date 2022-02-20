bc17b0edc84257f6303e96a08d63896570978ec7
charlyli17 <kilarichristine@gmail.com>
2022-02-20 16:27:36 +0100

    Commit attribut String file, méthode generePersonnality

--- src/engine/process/EvolutionElementManager.java
+++ src/engine/process/EvolutionElementManager.java
@@ -15,6 +15,8 @@ import data.Individu;
  */
 public class EvolutionElementManager {
 	private ArrayList<Individu> individus = new ArrayList<Individu>();
+	String file = "./src/fichiers/noms individus social.csv";
+	File f = new File(file);
 
 	public static int getRandomNumber(int min, int max) {
 		return (int) (Math.random() * (max + 1 - min)) + min;
@@ -27,7 +29,16 @@ public class EvolutionElementManager {
 	public void add(Individu individu) {
 		individus.add(individu);
 	}
-
+	
+	/**
+	 * genere un individu
+	 * @throws IOException
+	 */
+	public void generateIndividu() throws IOException {
+		Individu individu = new Individu(generateNom(f), generatePrenom(f), EvolutionElementManager.getRandomNumber(0, 100));
+		add(individu);
+	}
+	
 	/**
 	 * genere un nom de famille
 	 * 
@@ -36,7 +47,7 @@ public class EvolutionElementManager {
 	 * @return 
 	 * @throws IOException
 	 */
-	public static String generateNom(File csvfile) throws IOException {
+	public String generateNom(File csvfile) throws IOException {
 		String line = "";
 		ArrayList<String> noms = new ArrayList<String>();
 
@@ -65,7 +76,7 @@ public class EvolutionElementManager {
 	 * @return 
 	 * @throws IOException
 	 */
-	public static String generatePrenom(File csvfile) throws IOException {
+	public String generatePrenom(File csvfile) throws IOException {
 		String line = "";
 		ArrayList<String> prenoms = new ArrayList<String>();
 		
@@ -84,7 +95,32 @@ public class EvolutionElementManager {
 		int nombre = EvolutionElementManager.getRandomNumber(0, prenoms.size() - 1);
 		return prenoms.get(nombre);
 	}
-
 	
+	/**
+	 * genere une personnalité
+	 * @param csvfile
+	 * @return
+	 * @throws IOException
+	 */
+	public String generatePersonnality(File csvfile) throws IOException {
+		String line = "";
+		ArrayList<String> personnalities = new ArrayList<String>();
+		
+			try {
+				BufferedReader br = new BufferedReader(new FileReader(csvfile));
+
+				while ((line = br.readLine()) != null) {
+					String[] elm = line.split(",");
+					if (!elm[2].equals("Personnalité")) {
+						personnalities.add(elm[2]);
+					}
+				}
+			} catch (FileNotFoundException e) {
+				System.err.println("Fichier non trouvé");
+			}
+		int nombre = EvolutionElementManager.getRandomNumber(0, personnalities.size() - 1);
+		return personnalities.get(nombre);
+	}
+
 
 }
