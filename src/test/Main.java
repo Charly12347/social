6b0a7fa520a8feca91788bb2d9a35f2b9979adf7
charlyli17 <kilarichristine@gmail.com>
2022-02-21 23:24:45 +0100

    Commit essaie

--- src/test/Main.java
+++ src/test/Main.java
@@ -8,13 +8,15 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		String file = "./src/fichiers/noms individus social.csv";
+		String filepersonnality = "./src/fichiers/personnality.csv";
 		File f = new File(file);
+		File fper = new File(filepersonnality);
 		
 		for(int i=0; i<100; i++) {
 			String nom = EvolutionElementManager.generateNom(f);
 			String prenom = EvolutionElementManager.generatePrenom(f);
-			//String personnality = EvolutionElementManager.generatePersonnality(f);
-			System.out.println("Last name = " + nom + " ,First Name = " + prenom +" ,Personnality = ");
+			String personnality = EvolutionElementManager.generatePersonnality(fper);
+			System.out.println("Last name = " + nom + " ,First Name = " + prenom +" ,Personnality = " + personnality);
 		}
 	}
 }
